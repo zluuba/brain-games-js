be8a4aa9553aba25a4a472336b7a55d39dd85de0
zluuba <zluyba.nikitina@gmail.com>
2023-06-21 10:46:46 +0300

    fix: update greetUser func

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,8 @@ const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+
+  return name;
 };
 
 export default greetUser;
