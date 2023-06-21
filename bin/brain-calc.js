691a433f1510ea2247cb391d45fdd3809c652aa5
zluuba <zluyba.nikitina@gmail.com>
2023-06-21 11:56:48 +0300

    fix: update game scripts

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,56 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
+import { startCalcGame } from './games/brain-calc.js';
+import gameCore from '../src/index.js';
 
-const getRandomInt = () => {
-  const maxNumber = 100;
-  return Math.floor(Math.random() * maxNumber);
-};
-
-const getOperatorAndRightAnswer = (num1, num2) => {
-  const operations = [{
-    sign: '+',
-    method: function(num1, num2) { return num1 + num2; }
-  }, {
-    sign: '-',
-    method: function(num1, num2) { return num1 - num2; }
-  }, {
-    sign: '*',
-    method: function(num1, num2) { return num1 * num2; }
-  }];
-
-  const selectedOperator = Math.floor(Math.random() * operations.length);
-  const operator = operations[selectedOperator].sign;
-  const rightAnswer = operations[selectedOperator].method(num1, num2);
-
-  return [operator, rightAnswer];
-}
-
-const startCalcGame = (name, rule) => {
-  console.log(rule);
-  const rounds = 3;
-
-  for (let i = 0; i < rounds; i += 1) {
-    const num1 = getRandomInt();
-    const num2 = getRandomInt();
-    const [operator, rightAnswer] = getOperatorAndRightAnswer(num1, num2);
-
-    const userAnswer = readlineSync.question(`Question: ${num1} ${operator} ${num2} `);
-    console.log(`Your answer: ${userAnswer}`);
-
-    if (rightAnswer === Number(userAnswer)) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return false;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
-  return true;
-};
-
-const name = greetUser();
 const rule = 'What is the result of the expression?';
-startCalcGame(name, rule);
+gameCore(startCalcGame, rule);
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
+import { startEvenGame } from './games/brain-even.js';
+import gameCore from '../src/index.js';
 
-const getRandomInt = () => {
-  const maxNumber = 100;
-  return Math.floor(Math.random() * maxNumber);
-};
-
-const startEvenGame = (name, rule) => {
-  console.log(rule);
-  const rounds = 3;
-
-  for (let i = 0; i < rounds; i += 1) {
-    const currNum = getRandomInt();
-    const rightAnswer = currNum % 2 === 0 ? 'yes' : 'no';
-    const userAnswer = readlineSync.question(`Question: ${currNum} `);
-    console.log(`Your answer: ${userAnswer}`);
-
-    if (rightAnswer === userAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return false;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
-  return true;
-};
-
-const name = greetUser();
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-startEvenGame(name, rule);
+gameCore(startEvenGame, rule);
