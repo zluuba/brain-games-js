6a07aa159a7442807b72b503adde02606e8f72f6
zluuba <zluyba.nikitina@gmail.com>
2023-07-11 09:24:48 +0300

    fix: update even game

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,11 +3,13 @@ import core from '../index.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const evenGame = () => {
-  const currNum = getRandomInt(0, 100);
-  const rightAnswer = currNum % 2 === 0 ? 'yes' : 'no';
+const isEven = (number) => (number % 2 === 0);
 
-  return [currNum, rightAnswer];
+const evenGame = () => {
+  const number = getRandomInt(0, 100);
+  const rightAnswer = isEven(number) ? 'yes' : 'no';
+
+  return [number, rightAnswer];
 };
 
 export default () => core(evenGame, rule);
